967483e924a021fe9206e01db3367c23bbfc8919
Ashraf266 <mohamedashrafece@gmail.com>
2024-04-08 14:14:20 +0200

    Init of the project

---
+++ MDK-ARM/RTE/_Seat_Heater_Control_System/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Seat Heater Control System' 
+ * Target:  'Seat Heater Control System' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
